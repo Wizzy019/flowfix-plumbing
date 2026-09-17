d4a3209c3f616fbb6533dacd1bf84600bb668197
Wizzy019 <kingwizzyndukwu@gmail.com>
2026-09-17 11:03:26 +0100

    Made floating contact btn appear on every screen

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 import { ScrollToTop } from "../components/common/ScrollToTop";
+import FloatingContactButton from "../components/common/FloatingContactButton";
 
 export default function MainLayout() {
   return (
@@ -10,6 +11,7 @@ export default function MainLayout() {
       <Navbar />
       <main>
         <Outlet />
+        <FloatingContactButton />
       </main>
       <Footer />
     </>
